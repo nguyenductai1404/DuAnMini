f9720839306c1d3996640f445d7980c051962058
Đức Tài <taindpc00961@fpt.edu.vn>
2021-09-03 14:31:22 +0700

    Commit đầu tiên

---
+++ src/main/java/com/test/Taindpc00961Java6AsmApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taindpc00961Java6AsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taindpc00961Java6AsmApplication.class, args);
+	}
+
+}
